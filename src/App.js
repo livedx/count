7000276f7e4c26798ca8ef8ad5bc5789ebd39427
dmitriy <kolobokde@mail.ru>
2020-06-01 16:54:55 +0300

    counter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Fcomponent from "./Fcomponent";
+import Ccomponent from "./Ccomponent";
+
+function App() {
+    return(
+        <div className="App">
+            <Ccomponent/>
+        </div>
+    )
+}
+export default App
