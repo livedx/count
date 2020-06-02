e527f0d2599c06f7b39fcca0b91d43d74339f0ae
dmitriy <kolobokde@mail.ru>
2020-06-02 14:12:59 +0300

    faster todoList

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -5,36 +5,39 @@ export default class Ccomponent extends Component{
         super(props)
 
         this.state={
-            count: 0
+            input: '',
+            items: []
         }
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-        this.reset = this.reset.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
 
-    increment () {
-        this.setState(state => ({
-            count: state.count + 1
-        }) );
-    }
-    decrement () {
-        this.setState(state => ({
-            count: state.count - 1
-        }) );
-    }
-    reset () {
+    handleChange(event){
         this.setState({
-            count: 0
+            input: event.target.value
+        })
+    }
+    handleSubmit(event){
+        event.preventDefault()
+        this.setState({
+            input: this.state.input,
+            items: [...this.state.items, this.state.input]
         })
     }
 
     render() {
         return(
             <div>
-                <h1>Current:  {this.state.count}</h1>
-                <button onClick={this.increment}>increment</button>
-                <button onClick={this.decrement}>decrement</button>
-                <button onClick={this.reset}>reset</button>
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.input} onChange={this.handleChange}/>
+                    <button type="submit">Submit</button>
+                </form>
+                <ul>
+                    {this.state.items.map((item, index) =>(
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
